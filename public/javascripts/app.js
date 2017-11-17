1e81ec4528bfab119066cc5c7747baaff9905122
Jordan Mattingley <jmattingley23@gmail.com>
2017-11-16 19:28:49 -0700

    trying to save birthdays to mongo

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,9 +1,31 @@
 angular.module('birthdaytracker', [])
 .controller('MainCtrl', [
-  '$scope',
-  function($scope) {
-    $scope.addComment = function() {
-      $scope.formContent='';
+  '$scope', '$http',
+  function($scope, $http) {
+    $scope.birthdays = [];
+
+    $scope.addBirthday = function() {
+      if($scope.formContent === '') {
+        return;
+      }
+      console.log('In add birthday');
+      $scope.create({
+        date: $scope.formContent,
+      });
+      $scope.formContent = '';
+    }
+
+    $scope.getAll = function() {
+      return $http.get('/birthdays').success(function(data) {
+        angular.copy(data, $scope.birthdays);
+      });
+    }
+    $scope.getAll();
+
+    $scope.create = function(birthday) {
+      return $http.post('/birthdays', birthday).success(function(data) {
+        $scope.birthdays.push(data);
+      });
     }
   }
 ]);
