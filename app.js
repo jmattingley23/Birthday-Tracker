4d6adc2d9a025ff02d3f16dcb78694bdfcc5727a
Jordan Mattingley <jmattingley23@gmail.com>
2017-11-16 20:11:29 -0700

    new db

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/commentDB',{useMongoClient:true});
+mongoose.connect('mongodb://localhost/birthdayDB',{useMongoClient:true});
 require('./models/Birthday');
 
 var routes = require('./routes/index');
